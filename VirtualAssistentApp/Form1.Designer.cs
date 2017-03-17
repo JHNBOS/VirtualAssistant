dfc221dc16ad4b471338585ae277d5726a9852a8
Johan Bos <bosbosjohan@gmail.com>
2017-03-17 08:54:43 +0100

    VirtualAssistent Commit #1

---
+++ VirtualAssistentApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace VirtualAssistentApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.title = new System.Windows.Forms.Label();
+            this.weatherBox = new System.Windows.Forms.PictureBox();
+            this.internetBox = new System.Windows.Forms.PictureBox();
+            this.selfieBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.weatherBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.internetBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selfieBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.title.Font = new System.Drawing.Font("Quicksand", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(13, 13);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(532, 54);
+            this.title.TabIndex = 0;
+            this.title.Text = "Virtual Assistent";
+            this.title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // weatherBox
+            // 
+            this.weatherBox.BackColor = System.Drawing.Color.Transparent;
+            this.weatherBox.BackgroundImage = global::VirtualAssistentApp.Properties.Resources.cloud;
+            this.weatherBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.weatherBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("weatherBox.InitialImage")));
+            this.weatherBox.Location = new System.Drawing.Point(31, 93);
+            this.weatherBox.Name = "weatherBox";
+            this.weatherBox.Size = new System.Drawing.Size(123, 119);
+            this.weatherBox.TabIndex = 1;
+            this.weatherBox.TabStop = false;
+            // 
+            // internetBox
+            // 
+            this.internetBox.BackColor = System.Drawing.Color.Transparent;
+            this.internetBox.BackgroundImage = global::VirtualAssistentApp.Properties.Resources.internet;
+            this.internetBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.internetBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("internetBox.InitialImage")));
+            this.internetBox.Location = new System.Drawing.Point(220, 93);
+            this.internetBox.Name = "internetBox";
+            this.internetBox.Size = new System.Drawing.Size(123, 119);
+            this.internetBox.TabIndex = 2;
+            this.internetBox.TabStop = false;
+            // 
+            // selfieBox
+            // 
+            this.selfieBox.BackColor = System.Drawing.Color.Transparent;
+            this.selfieBox.BackgroundImage = global::VirtualAssistentApp.Properties.Resources.photo_camera;
+            this.selfieBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.selfieBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("selfieBox.InitialImage")));
+            this.selfieBox.Location = new System.Drawing.Point(407, 93);
+            this.selfieBox.Name = "selfieBox";
+            this.selfieBox.Size = new System.Drawing.Size(123, 119);
+            this.selfieBox.TabIndex = 3;
+            this.selfieBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(557, 455);
+            this.Controls.Add(this.selfieBox);
+            this.Controls.Add(this.internetBox);
+            this.Controls.Add(this.weatherBox);
+            this.Controls.Add(this.title);
+            this.Font = new System.Drawing.Font("Quicksand", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Virtual Assistent";
+            ((System.ComponentModel.ISupportInitialize)(this.weatherBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.internetBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selfieBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.PictureBox weatherBox;
+        private System.Windows.Forms.PictureBox internetBox;
+        private System.Windows.Forms.PictureBox selfieBox;
+    }
+}
+
---
+++ VirtualAssistentApp/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+using Emgu.CV;
+using System.Drawing.Imaging;
+using System.Xml;
+using System.IO;
+using System.Threading;
+
+namespace VirtualAssistentApp
+{
+    public partial class Form1 : Form
+    {
+        //OBJECTS
+        private SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        private SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        private Capture capture;
+        private HandleProcess handler = new HandleProcess();
+        private Bitmap image;
+
+        //PRIMITIVE TYPES
+        private string userName { get; set; }
+        private string temperature { get; set; }
+        private string condition { get; set; }
+        private string City { get; set; }
+        private string Query { get; set; }
+        private bool minimized { get; set; }
+        private bool awake = false;
+        private bool selfie = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //INITIALIZING VARIABLES
+            City = "Rotterdam";
+            userName = "Johan Bos";
+            string fname = userName.Split(' ')[0];
+            minimized = false;
+
+            //RUN METHODS
+            setupSpeechRecognition();
+
+            //START RECOGNIZING
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+            //SAY WELCOME TEXT
+            synthesizer.SpeakAsync("Hello " + fname + ", how may I help you?");
+
+        }
+
+        //--------------------------------------------------------------------------------------------//
+        // METHODS
+
+        private void setupSpeechRecognition()
+        {
+            //RECOGNIZER
+            RecognizerInfo recognizerInfo = null;
+            foreach (RecognizerInfo ri in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                if ((ri.Culture.TwoLetterISOLanguageName.Equals("en")) && (recognizerInfo == null))
+                {
+                    recognizerInfo = ri;
+                    break;
+                }
+            }
+
+            //SET VOICE
+            synthesizer.SelectVoiceByHints(VoiceGender.Female);
+
+            //SELECT MICROPHONE
+            recEngine.SetInputToDefaultAudioDevice();
+
+            //SETUP POSSIBLE COMMANDS
+            Choices commands = new Choices();
+            commands.Add(File.ReadAllLines(@"D:\Johan Bos\Documents\Visual Studio 2015\Projects\PersonalProjects\VirtualAssistentApp\commands.txt"));
+
+            //GRAMMARBUILDER
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Culture = recognizerInfo.Culture;
+            gBuilder.Append(commands);
+
+            //GRAMMAR
+            Grammar grammar = new Grammar(gBuilder);
+
+            //RECOGNITION ENGINE
+            recEngine.RequestRecognizerUpdate();
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+        }
+
+        //EXIT PROGRAM
+        private void Exit()
+        {
+            synthesizer.Speak("Have a nice day");
+            Environment.Exit(0);
+        }
+
+        //OPEN DEFAULT BROWSER
+        private void openBrowser()
+        {
+            synthesizer.Speak("Starting browser ");
+            Process.Start("http:\\www.google.nl");
+        }
+
+        //MINIMIZE OR MAXIMIZE BROWSER
+        private void minimizeBrowser()
+        {
+            if (minimized)
+            {
+                minimized = false;
+                handler.CloseWindow("chrome");
+                handler.CloseWindow("firefox");
+                handler.CloseWindow("ie");
+            }
+            else
+            {
+                minimized = true;
+                handler.CloseWindow("chrome");
+                handler.CloseWindow("firefox");
+                handler.CloseWindow("ie");
+            }
+
+        }
+
+        //GET WEATHER FROM YAHOO
+        private String GetWeather(String input)
+        {
+            String query = String.Format("https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + City + "')&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+            XmlDocument wData = new XmlDocument();
+            try
+            {
+                wData.Load(query);
+            }
+            catch
+            {
+                MessageBox.Show("No Internet connection!");
+                return "No internet";
+            }
+            XmlNamespaceManager manager = new XmlNamespaceManager(wData.NameTable);
+            manager.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+            XmlNode channel = wData.SelectSingleNode("query").SelectSingleNode("results").SelectSingleNode("channel");
+            XmlNodeList nodes = wData.SelectNodes("query/results/channel");
+            try
+            {
+                temperature = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["temp"].Value;
+                condition = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["text"].Value;
+
+                if (input == "temp")
+                {
+                    return temperature;
+                }
+
+                if (input == "cond")
+                {
+                    return condition;
+                }
+            }
+            catch
+            {
+                return "Error Receiving data";
+            }
+            return "error";
+        }
+
+        //--------------------------------------------------------------------------------------------//
+        // LISTENERS
+
+        //SPEECH RECOGNIZER LISTENER
+        private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            if (e.Result.Text == "hey V A")
+            {
+                awake = true;
+            }
+
+            else if (awake == true)
+            {
+                switch (e.Result.Text)
+                {
+                    case "what date is it":
+                        synthesizer.Speak("Today is " + DateTime.Now.ToString("dd MMMM"));
+                        break;
+
+                    case "what time is it":
+                        synthesizer.Speak("The time is " + DateTime.Now.ToString("hh mm"));
+                        break;
+
+                    case "whats the weather like":
+                        synthesizer.Speak("The sky is " + GetWeather("cond").ToLower());
+                        break;
+
+                    case "whats the temperature":
+                        string temperature = GetWeather("temp");
+                        double temp = 5.0 / 9.0 * (double.Parse(temperature) - 32);
+
+                        synthesizer.Speak("The temperature is " + Math.Round(temp, 1) + " degrees Celsius");
+                        break;
+
+                    case "go to internet":
+                    case "go to google":
+                        openBrowser();
+                        break;
+
+                    case "open internet":
+                    case "open browser":
+                        minimizeBrowser();
+                        break;
+
+                    case "close internet":
+                    case "close browser":
+                        minimizeBrowser();
+                        break;
+
+                    case "take a selfie":
+                    case "selfie":
+                        capture = new Capture();
+                        selfie = true;
+                        Application.Idle += Application_Idle;
+                        break;
+
+                    case "close":
+                    case "exit":
+                        Exit();
+                        break;
+
+                    default:
+                        break;
+                }
+
+                awake = false;
+            }
+        }
+
+        //LISTENER TO TAKE SELFIE FROM WEBCAM
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            if (selfie)
+            {
+                synthesizer.Speak("Say cheese in 3");
+                Thread.Sleep(1);
+                synthesizer.Speak("2");
+                Thread.Sleep(1);
+                synthesizer.Speak("1");
+
+                image = capture.QueryFrame().Bitmap;
+                image.Save(@"C:/VirtualAssistant/Selfie-" + DateTime.Now.ToString("ddhhmm") + ".png", ImageFormat.Png);
+
+                Thread.Sleep(500);
+
+                synthesizer.Speak("Your picture is taken");
+
+                selfie = false;
+            }
+            
+        }
+
+        //----------------------------------------------------------------------------------------//
+
+
+    }
+}
